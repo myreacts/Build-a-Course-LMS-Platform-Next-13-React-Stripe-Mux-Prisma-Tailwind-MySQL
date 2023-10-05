e2d0e7c8c1c800a68fb6c656027ea14058c5928b
baizhi958216 <1475289190@qq.com>
2023-10-05 19:36:46 +0800

    feat: 路由解释
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ app/(group)/antonio/page.tsx
@@ -0,0 +1,5 @@
+const AntonioPage = () => {
+  return <div>AntonioPage AntonioPage</div>;
+};
+
+export default AntonioPage;
---
+++ app/(group)/routea/page.tsx
@@ -0,0 +1,5 @@
+const RouteA = () => {
+  return <div>This is a route inside a group folder</div>;
+};
+
+export default RouteA;
