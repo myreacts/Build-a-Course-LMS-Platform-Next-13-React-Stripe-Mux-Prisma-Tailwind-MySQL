faa30a146a8a6e97c40a933f6dce0cc972887664
baizhi958216 <1475289190@qq.com>
2023-10-17 23:53:03 +0800

    feat: 课程发布和取消
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -15,6 +15,8 @@ import CategoryForm from "./_components/category-form";
 import PriceForm from "./_components/price-form";
 import AttachmentForm from "./_components/attachment-form";
 import ChaptersForm from "./_components/chapters-form";
+import { Banner } from "@/components/banner";
+import { Actions } from "./_components/actions";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const userId = auth();
@@ -63,8 +65,12 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 
   const completionText = `(${compeletedFields} / ${totalFields})`;
 
+  const isCompleted = requiredFields.every(Boolean);
   return (
     <>
+      {!course.isPublished && (
+        <Banner label="This course is unpublish. It will not be visible to the students." />
+      )}
       <div className="p-6">
         <div className="flex items-center justify-between">
           <div className="flex flex-col gap-y-2">
@@ -73,6 +79,11 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
               Complete all fields {completionText}
             </span>
           </div>
+          <Actions
+            disabled={!isCompleted}
+            courseId={params.courseId}
+            isPublished={course.isPublished}
+          />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
           <div>
