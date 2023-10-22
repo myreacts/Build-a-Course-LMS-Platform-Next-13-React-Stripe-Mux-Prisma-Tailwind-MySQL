be8ac13ea8dc347c97b34c300d3c810c0a55e89a
baizhi958216 <1475289190@qq.com>
2023-10-22 23:29:57 +0800

    feat: 教师统计柱状图
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -2,6 +2,7 @@ import { getAnalytics } from "@/actions/get-analytics";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { DataCard } from "./_components/data-card";
+import { Chart } from "./_components/chart";
 
 const AnalyticsPage = async () => {
   const { userId } = auth();
@@ -16,6 +17,7 @@ const AnalyticsPage = async () => {
         <DataCard value={totalRevenue} shouldFormat label="Total Revenue" />
         <DataCard value={totalSales} label="Total Sales" />
       </div>
+      <Chart data={data} />
     </div>
   );
 };
