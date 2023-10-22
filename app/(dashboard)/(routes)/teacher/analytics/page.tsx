367d0d4d50c4d0931eed8a1fecd9a9ceb0be990b
baizhi958216 <1475289190@qq.com>
2023-10-22 23:23:36 +0800

    feat: 教师统计页
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,5 +1,23 @@
-const AnalyticsPage = () => {
-  return <>Analytics</>;
+import { getAnalytics } from "@/actions/get-analytics";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { DataCard } from "./_components/data-card";
+
+const AnalyticsPage = async () => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+  return (
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard value={totalRevenue} shouldFormat label="Total Revenue" />
+        <DataCard value={totalSales} label="Total Sales" />
+      </div>
+    </div>
+  );
 };
 
 export default AnalyticsPage;
